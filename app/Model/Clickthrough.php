3783470e51e5bc55f5c1bc38a856d9629ee8c9e1
Aleksey Klyuev <zavodnoi@gmail.com>
2020-05-26 20:25:30 +0300

    Коммит добавит связь  clickthroughs модели ShortLink.php

--- app/Model/Clickthrough.php
+++ app/Model/Clickthrough.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Clickthrough extends Model
@@ -11,4 +12,14 @@ class Clickthrough extends Model
     protected $guarded = ['id'];
 
     protected $dates = ['visited_at'];
+
+    /**
+     * @param $visited_at
+     * @return string
+     */
+    public function getVisitedAtAttribute($visited_at): string
+    {
+        return Carbon::parse($visited_at)->format('d.m.Y H:i:s');
+    }
+
 }
