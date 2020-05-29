6d4b45486ff99ff66438c6cc6b29385d5fb8193d
Aleksey Klyuev <zavodnoi@gmail.com>
2020-05-29 18:20:56 +0300

    Коммит удалит метод getLocationInfo из интерфейса LocationInfoInterface.php

--- app/Contracts/LocationInfoInterface.php
+++ app/Contracts/LocationInfoInterface.php
@@ -6,8 +6,6 @@ namespace App\Contracts;
 
 interface LocationInfoInterface
 {
-    public function getLocationInfo(): array ;
-
     public function getCity(): string;
 
     public function getCountry(): string;
